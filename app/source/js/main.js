94b10533dd6a8986e08473fc22877a957f04833b
Victoria Kravchenko <victoria.kr.1990@gmail.com>
2016-02-03 13:24:56 +0200

    validate bpopup beta

--- app/source/js/main.js
+++ app/source/js/main.js
@@ -1,17 +1,71 @@
 
 $(document).ready(function(){
     
-    console.log(1);
+    // nav
+    
     $(".mobile_menu").on("click", function(){
         $(this).children('.navigation_mob').toggleClass("open");
     });
-//    $(".mobile_menu").on("click", function(){
-//        $('.navigation_mob').toggleClass("open");      
-//        return false; 
-//    });
 
+    // popup
     
-  });
+    $('.new_project').on('click', function(){
+        $('.pop_up').bPopup({
+            speed:500,
+            opasity:.3,
+            positionStyle: 'fixed',
+            transition: 'slideDown'
+        })
+        return false;
+    });
+    
+    $('.b-closse').on('click', function(){
+        var bPopup = $('.pop_up').bPopup();
+        bPopup.close();
+        reset_validate();
+    });
+    
+    //validate
+    
+    $('form').validate({
+        rules:{
+            name_project_form:'required',
+            image_project_form:'required',
+            url_project_form:'required',
+            message_project:'required',
+            username:'required',
+            mail:'required',
+            message:'required',
+            captcha:'required'
+        },messages:{
+            name_project_form:'введите название',
+            image_project_form:'изображение',
+            url_project_form:'ссылка на проект',
+            message_project:'описание проекта',
+            username:'введите имя',
+            mail:'введите email',
+            message:'ваш вопрос',
+            captcha:'код капчи'
+        }
+        
+    });
+    
+//    (function err_left(){
+//        $('.form_float_block').find('.error').addClass('error_left');
+//        console.log(1);
+//    });
+    
+    function reset_validate(){
+        $('.pop_up').find("label.error").remove();
+        $('.pop_up').find('.error').removeClass('error');
+        
+    };
+    
+    
+    
+    
+
+});
 
 // image_project_form
 
@@ -26,3 +80,5 @@ function getName (str){
     var uploaded = document.getElementById("fileformlabel");
     uploaded.innerHTML = filename;
 }
+
+
